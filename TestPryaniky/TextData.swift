31512dbdcf74c2ebe21fd2a7525fadf137e1ff71
Vladislav Markov <lerd.95@mail.ru>
2019-04-03 23:30:42 +0300

    Add TextData

---
+++ TestPryaniky/TextData.swift
@@ -0,0 +1,11 @@
+//
+//  TextData.swift
+//  TestPryaniky
+//
+//  Created by Vladislav on 03/04/2019.
+//  Copyright © 2019 Vladislav Markov. All rights reserved.
+//
+
+struct TextData: Codable {
+    var text: String
+}
