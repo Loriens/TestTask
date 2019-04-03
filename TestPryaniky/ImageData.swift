88ae701689088557116e404c47578dedf2ee8d6f
Vladislav Markov <lerd.95@mail.ru>
2019-04-03 23:34:59 +0300

    Add ImageData

---
+++ TestPryaniky/ImageData.swift
@@ -0,0 +1,12 @@
+//
+//  ImageData.swift
+//  TestPryaniky
+//
+//  Created by Vladislav on 03/04/2019.
+//  Copyright © 2019 Vladislav Markov. All rights reserved.
+//
+
+struct ImageData: Codable {
+    var text: String
+    var imageURL: String
+}
